1fea82e2f5b183d8af383fb08b0c1df83736c133
Kristian Freeman <kristian@kristianfreeman.com>
2020-08-25 10:22:54 -0500

    Parse members JSON

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import redirector from 'lilredirector'
-
 import ϟ from 'ggf'
 
 addEventListener('fetch', ev => ev.respondWith(handler(ev)))
+
+const deferScriptWhitelist = [
+  'assets/js/app.min.js',
+  'assets/vendor.min',
+  'assets/ghost.min',
+]
+
+const embedJson = data => ({
+  element: element => {
+    element.append(
+      `<script id="members_data" type="application/json">${JSON.stringify(
+        data,
+      )}</script>`,
+      {
+        html: true,
+      },
+    )
+  },
+})
 
 async function handler(event) {
   try {
@@ -17,14 +35,21 @@ async function handler(event) {
       ENVIRONMENT === 'development'
         ? new Request(ORIGIN + url.pathname)
         : request
-    const resp = await fetch(req)
 
-    return ϟ(resp, {
-      deferScripts: {
-        enabled: true,
-        options: { except: ['/assets/js/app.min.js'] },
-      },
-    })
+    const resp = await fetch(req)
+    if (url.pathname.startsWith('/ghost')) {
+      return resp
+    } else if (url.pathname.startsWith('/members')) {
+      const data = await (await fetch(ORIGIN + '/members.json')).json()
+      return new HTMLRewriter().on('body', embedJson(data)).transform(resp)
+    } else {
+      return ϟ(resp, {
+        deferScripts: {
+          enabled: true,
+          options: { except: deferScriptWhitelist },
+        },
+      })
+    }
   } catch (err) {
     console.log(err.message)
     return new Response(err.toString())
